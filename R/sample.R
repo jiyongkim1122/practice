40ee95f5a8ff861e78c1c1eec90c426fd6a829f7
jiyongkim1122 <rabbi0726@naver.com>
2017-12-26 17:42:33 +0900

    Do you wanna Die?

---
+++ R/sample.R
@@ -0,0 +1,12 @@
+#' Function of square value.
+#'
+#' @param x input integer
+#' @return Value of squared
+#' @example
+#' my_sample(x)
+#' @export
+
+my_sample = function(x){
+  y = x^2
+  return(y)
+}
